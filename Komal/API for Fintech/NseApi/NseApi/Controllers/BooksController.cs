d4b1227e2cd21ee6d48dce037ce15ba0b3f9eec4
komal-wayse <komalwayse10@gmail.com>
2021-02-02 17:22:03 +0530

    NSE Api
    Create a nse api

---
+++ Komal/API for Fintech/NseApi/NseApi/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NseApi.Models;
+using NseApi.Services;
+
+
+namespace NseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get() =>
+            _bookService.Get();
+
+        //[HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+       
+
+       
+    }
+}
+    
